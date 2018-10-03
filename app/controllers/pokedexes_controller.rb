c0aa858fc454235743f6c518910c0bd7725600a4
nisaangelina <nisa.angelina@ukirama.com>
2018-10-03 07:24:23 +0700

    CRUD for pokedex

---
+++ app/controllers/pokedexes_controller.rb
@@ -0,0 +1,48 @@
+class PokedexesController < ApplicationController
+  def index
+    @pokedexes = ::Pokedex.all
+  end
+
+  def show
+   @pokedexes = ::Pokedex.find(params[:id])
+  end
+
+  def new
+    @pokedex = ::Pokedex.new
+  end
+
+  def create
+    @pokedex = ::Pokedex.new(pokedex_params)
+     if @pokedex.save
+
+      flash[:success] = "Pokedex '#{@pokedex.name}' Added!"
+      redirect_to pokedexes_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @pokedex = ::Pokedex.find(params[:id])
+  end
+
+  def update
+    @pokedex = ::Pokedex.find(params[:id])
+    if @pokedex.update(pokedex_params)
+      flash[:success] = "Pokedex '#{@pokedex.name}' Updated!"
+      redirect_to pokedexes_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pokedex = ::Pokedex.find(params[:id])
+    @pokedex.destroy
+    redirect_to pokedexes_path
+  end
+
+ def pokedex_params
+  params.require(:pokedex).permit(:name, :base_health_point, :base_attack, :base_defence, :base_speed, :element_type, :image_url)
+ end
+end
